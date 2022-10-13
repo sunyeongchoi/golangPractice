2dd224ddb63cf7d5c3ac456f40225571f1cff7be
sn0716 <sn0716@naver.com>
2022-10-13 09:09:39 +0900

    Unbuffered Channels

--- main.go
+++ main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
-// Go channel
-func Calculate(c chan int)  {
-	c <- rand.Intn(10)
+// Go channel - Unbuffered Channels
+func Calculate(c chan int) {
+	value := rand.Intn(10)
+	fmt.Println("Calculated Random Value: {}", value)
+	time.Sleep(1000 * time.Millisecond)
+	c <- value
+	fmt.Println("Only Executes after another goroutine performs a receive on the channel")
 }
-func main()  {
+func main() {
 	fmt.Println("Go Channel Tutorial")
 	values := make(chan int)
 	defer close(values)
 
 	go Calculate(values)
-	value := <- values
+	go Calculate(values)
+
+	value := <-values
 	fmt.Println(value)
 }
