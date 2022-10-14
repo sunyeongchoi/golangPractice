2cd3c36ba376bc29089f012179dcf2e9603f9dc1
sn0716 <sn0716@naver.com>
2022-10-14 09:54:37 +0900

    waitgroup anonymous function

--- main.go
+++ main.go
@@ -3,24 +3,20 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 // WaitGroups
-
-func myFunc(wg *sync.WaitGroup) {
-	time.Sleep(1 * time.Second)
-	fmt.Println("Inside my goroutine")
-	// signal the end of its' execution
-	wg.Done()
-}
 
 func main() {
 	fmt.Println("Hello World")
 	var waitgroup sync.WaitGroup
 	// set the number of goroutines
 	waitgroup.Add(1)
-	go myFunc(&waitgroup)
+	go func() {
+		fmt.Println("Inside my goroutine")
+		// signal the end of its' execution
+		waitgroup.Done()
+	}()
 	// block the execution of our main() function until the goroutines in the waitgroup have successfully completed
 	waitgroup.Wait()
 	fmt.Println("Finish Execution")
