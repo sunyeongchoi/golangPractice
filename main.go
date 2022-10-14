b855199d2662bb5051919700df18c1d3d43e5813
sn0716 <sn0716@naver.com>
2022-10-14 10:15:44 +0900

    waitgroup real example

--- main.go
+++ main.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"sync"
 )
 
 // WaitGroups
 
+var urls = []string{
+	"https://google.com",
+	"https://tutorialedge.net",
+	"https://twitter.com",
+}
+
+func fetch(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(response.Status)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home page endpoint hit")
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go fetch(url, &wg)
+	}
+	wg.Wait()
+	fmt.Println("Returning Response")
+	fmt.Fprintf(w, "Responses")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	fmt.Println("Hello World")
-	var waitgroup sync.WaitGroup
-	// set the number of goroutines
-	waitgroup.Add(1)
-	go func() {
-		fmt.Println("Inside my goroutine")
-		// signal the end of its' execution
-		waitgroup.Done()
-	}()
-	// block the execution of our main() function until the goroutines in the waitgroup have successfully completed
-	waitgroup.Wait()
-	fmt.Println("Finish Execution")
+	handleRequest()
 }
