390ec632ef4b984562003d54c183e85ddf8339ec
sn0716 <sn0716@naver.com>
2022-11-22 10:17:49 +0900

    go interface

---
+++ interfaces_in_go/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Employee interface {
+	GetName() string
+}
+
+type Engineer struct {
+	Name string
+}
+
+func (e *Engineer) GetName() string {
+	return "Engineer Name: " + e.Name
+}
+
+type Manager struct {
+	Name string
+}
+
+func (m Manager) GetName() string {
+	return "Manager Name: " + m.Name
+}
+
+func PrintDetails(e Employee)  {
+	fmt.Println(e.GetName())
+}
+
+func main() {
+	engineer := &Engineer{
+		Name: "snchoi",
+	}
+	manager := Manager{
+		Name: "ellie",
+	}
+	PrintDetails(engineer)
+	PrintDetails(manager)
+}
